a6036c7a82cdb39a9dc1cfb5c7af0dc4d70ae41b
Amman Sood <392075+asood92@users.noreply.github.com>
2020-10-19 17:09:57 -0400

    added a few comments

--- js/script.js
+++ js/script.js
@@ -5,18 +5,33 @@ $("#instruction-button").click(function(){
     });
 });
 
+//  startInhale()
+//  Use: replace the contents of the element at id #breathingStep with text
+//  Parameters: None
 function startInhale() {
     document.querySelector("#breathingStep").innerHTML = "Inhale through your nose";
     setTimeout(startInhale, 4000);
 }
+
+//  holdBreath()
+//  Use: replace the contents of the element at id #breathingStep with text
+//  Parameters: None
 function holdBreath() {
     document.querySelector("#breathingStep").innerHTML = "Hold your breath";
     setTimeout(holdBreath, 7000);
 }
+
+//  slowExhale()
+//  Use: replace the contents of the element at id #breathingStep with text
+//  Parameters: None
 function slowExhale() {
     document.querySelector("#breathingStep").innerHTML = "Exhale through your mouth";
     setTimeout(slowExhale, 8000);
 }
+
+//  startBreathing
+//  Use: Call all of the other functions for actual chained output
+//  Parameters: None
 function startBreathing() {
     startInhale();
     holdBreath();
